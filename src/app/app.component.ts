20d6faf78187642df044e7c30881944fa78850cd
Jasmina Messaoud <jasminamessaoud@hotmail.com>
2017-11-14 12:34:35 +0100

    Revert "Revert "fixed tripplanner page""
    This reverts commit c473a3273f91e7fbe304fd36426d4041a278fd3f.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { LevelingPage } from '../pages/leveling/leveling';
+import { TripplannerPage } from '../pages/tripplanner/tripplanner';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +23,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'Leveling', component: LevelingPage }
+      { title: 'Leveling', component: LevelingPage },
+      { title: 'Tripplanner', component: TripplannerPage }
     ];
 
   }
