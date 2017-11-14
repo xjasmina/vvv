b72b5151224790888384d218c203ff5ba1909857
Jasmina Messaoud <jasminamessaoud@hotmail.com>
2017-11-14 12:06:24 +0100

    added trip pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LevelingPage } from '../pages/leveling/leveling';
+import { LevelingPage } from '../pages/tripplanner/tripplanner';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
