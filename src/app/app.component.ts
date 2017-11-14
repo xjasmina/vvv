318bb20e9359d67a21abc5516c8fd4c3e85d46e0
Jasmina Messaoud <jasminamessaoud@hotmail.com>
2017-11-14 11:44:43 +0100

    added leveling page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LevelingPage } from '../pages/leveling/leveling';
 
 @Component({
   templateUrl: 'app.html'
@@ -23,7 +24,7 @@ export class MyApp {
     this.pages = [
       { title: 'Home', component: HomePage },
       { title: 'List', component: ListPage },
-      { title: 'List', component: ListPage }
+      { title: 'Leveling', component: LevelingPage }
     ];
 
   }
---
+++ src/pages/leveling/list.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-leveling',
+  templateUrl: 'leveling.html'
+})
+export class LevelingPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
