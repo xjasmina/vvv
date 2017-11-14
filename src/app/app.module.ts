3e428f3992080eeaf5b390674d2b22cf64fe5c63
Jasmina Messaoud <jasminamessaoud@hotmail.com>
2017-11-14 11:56:41 +0100

    list page removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-import { ListPage } from '../pages/leveling/leveling';
+import { LevelingPage } from '../pages/leveling/leveling';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,7 +13,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage,
     LevelingPage
   ],
   imports: [
@@ -25,7 +23,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage,
     LevelingPage
   ],
   providers: [
