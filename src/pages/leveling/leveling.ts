ce5b74d2da7d3aa282bfc2c9bef2ccca9af5318f
Jasmina Messaoud <jasminamessaoud@hotmail.com>
2017-11-15 14:52:55 +0100

    added leveling
    level gaat omhoog bij klik

--- src/pages/leveling/leveling.ts
+++ src/pages/leveling/leveling.ts
@@ -5,10 +5,34 @@ import { NavController } from 'ionic-angular';
   selector: 'page-leveling',
   templateUrl: 'leveling.html'
 })
+
 export class LevelingPage {
 
-  constructor(public navCtrl: NavController) {
 
-  }
+    constructor(public navCtrl: NavController) {
+   }
+
+
+      // start leveling on click
+      levelClick(){
+
+          let level = 0;
+
+
+          //store data
+          if(localStorage.level) level = localStorage.getItem('level');
+          document.getElementById("level").innerHTML = level;
+
+          // level up when clicked 
+          level = parseInt(level) + 1;
+          document.getElementById("level").innerHTML = level;
+          localStorage.setItem('level', level);
+
+          //test
+          console.log("Hello?");
+          
+      }
 
 }
+
+
