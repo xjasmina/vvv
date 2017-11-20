359ad581abdebcf7a9ed774c76723e763686c5a8
Jasmina Messaoud <jasminamessaoud@hotmail.com>
2017-11-20 14:46:41 +0100

    removed leveling

--- src/pages/leveling/leveling.ts
+++ src/pages/leveling/leveling.ts
@@ -8,29 +8,29 @@ import { Component } from '@angular/core';
 export class LevelingPage {
 
 
-      // start leveling on click
-      levelClick() {
+      // // start leveling on click
+      // levelClick() {
 
-          let level:string = 0;
+      //     let level: number = 0;
 
-          //store data
-          if(localStorage.level) level = localStorage.getItem('level');
-          document.getElementById('level').innerHTML = String(level);
+      //     //store data
+      //     if(localStorage.level) level = localStorage.getItem('level');
+      //     document.getElementById('level').innerHTML = level;
 
-          // level up when clicked 
-          level = Math.min(Math.max(parseInt(level), 0), 10);
-          level = level + 1;
+      //     // level up when clicked 
+      //     level = Math.min(Math.max(parseInt(level), 0), 10);
+      //     level = level + 1;
 
-          document.getElementById('level').innerHTML = String(level);
-          localStorage.setItem('level', level);
+      //     document.getElementById('level').innerHTML = level;
+      //     localStorage.setItem('level', level);
 
-          if (level = 1) {
-            console.log("wow nog maar level 1");
-          }
+      //     if (level == 1) {
+      //       console.log("wow nog maar level 1");
+      //     }
 
-          //localStorage.clear();
+      //     //localStorage.clear();
 
-      }
+      // }
 
 }
 
