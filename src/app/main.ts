180d96946c239a016af9ae3e62c82153ddaaf298
Jasmina Messaoud <jasminamessaoud@hotmail.com>
2018-01-10 11:33:09 +0100

    spul weggehaald

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,6 +2,4 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
-import 'web-animations-js/web-animations.min';
-
 platformBrowserDynamic().bootstrapModule(AppModule);
